fd64aeb79100bf737a908714ca6abc9adf73918d
Takeru Miyake <m18kecam777@gmail.com>
2024-10-30 00:23:36 +0900

    ✨ add nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function FeedScreen() {
-  return <Text>Feeds</Text>;
+  return (
+    <View className="bg-slate-600 flex-1 items-center justify-center m-10">
+      <Text className="text-3xl font-bold">Feeds</Text>
+    </View>
+  );
 }
