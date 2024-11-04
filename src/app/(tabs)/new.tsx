5dbdc61602910da8f0592724ded80e9159e64f39
Takeru Miyake <m18kecam777@gmail.com>
2024-11-04 21:13:21 +0900

    profile page

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Image, Pressable, Text, TextInput, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import Button from "~/src/components/Button";
 
 export default function CreatePost() {
   const [caption, setCaption] = useState<string>("");
@@ -52,9 +53,7 @@ export default function CreatePost() {
       />
 
       <View className="mt-auto w-full">
-        <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
-          <Text className="text-white font-semibold">Share</Text>
-        </Pressable>
+        <Button title="Share" />
       </View>
     </View>
   );
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,5 +1,58 @@
-import { Text } from "react-native";
+import { Text, View, Image, TextInput } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import Button from "~/src/components/Button";
 
 export default function ProfileScreen() {
-  return <Text>Profile</Text>;
+  const [image, setImage] = useState<string | null>(null);
+  const [username, setUsername] = useState<string>("");
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View className="p-2 flex-1">
+      {image ? (
+        <Image
+          source={{
+            uri: image,
+          }}
+          className="w-52 aspect-square rounded-full self-center"
+        />
+      ) : (
+        <View className="w-52 aspect-square rounded-full bg-slate-300 self-center" />
+      )}
+      <Text
+        className="text-blue-500 font-semibold m-5 self-center"
+        onPress={pickImage}
+      >
+        Change
+      </Text>
+
+      <Text className="mb-2 text-gray-500 font-semibold">Username</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        className="border border-gray-300 p-2 rounded-md"
+      />
+
+      <View className="gap-3 mt-auto">
+        <Button title="Update Profile" />
+        <Button title="Sign out" />
+      </View>
+    </View>
+  );
 }
